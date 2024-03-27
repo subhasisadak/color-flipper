b17e23a5ecc467187584dc5525d94fc2ea892a4f
subhasisadak <adaksubhasis2016@gmail.com>
2024-03-27 14:13:52 +0530

    optimised

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let background = document.querySelector(".background");
 let button = document.querySelector(".button");
 let codeArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-
-let changeColor = () => {
+function getNewColorCode() {
     let newColor = '#';
     for(let i = 0; i < 6; i++){
         newColor += codeArr[Math.floor(Math.random() * 16)];
     }
+    return newColor;
+}
+let changeColor = () => {
+    newColor = getNewColorCode();
     background.style.backgroundColor = newColor;
     button.style.color = newColor;
     console.log(newColor);
-    newColor = '#';
-    for(let i = 0; i < 6; i++){
-        newColor += codeArr[Math.floor(Math.random() * 16)];
-    }
+    newColor = getNewColorCode();
     button.style.backgroundColor = newColor;
     // let color = background.style.backgroundColor;
     console.log(newColor);
