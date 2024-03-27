9fb07490f7e226772d0261269300110dfbd98ec6
subhasisadak <adaksubhasis2016@gmail.com>
2024-03-27 14:09:45 +0530

    uploding final project

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ let button = document.querySelector(".button");
 let codeArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
 let changeColor = () => {
-    let number1 = codeArr[Math.floor(Math.random() * 16)];
-    let number2 = codeArr[Math.floor(Math.random() * 16)];
-    let number3 = codeArr[Math.floor(Math.random() * 16)];
-    let number4 = codeArr[Math.floor(Math.random() * 16)];
-    let number5 = codeArr[Math.floor(Math.random() * 16)];
-    let number6 = codeArr[Math.floor(Math.random() * 16)];
-    let newColor = '#' + number1 + number2 + number3 + number4 + number5 + number6;
+    let newColor = '#';
+    for(let i = 0; i < 6; i++){
+        newColor += codeArr[Math.floor(Math.random() * 16)];
+    }
     background.style.backgroundColor = newColor;
+    button.style.color = newColor;
+    console.log(newColor);
+    newColor = '#';
+    for(let i = 0; i < 6; i++){
+        newColor += codeArr[Math.floor(Math.random() * 16)];
+    }
+    button.style.backgroundColor = newColor;
     // let color = background.style.backgroundColor;
-    console.log('#' + number1 + number2 + number3 + number4 + number5 + number6);
+    console.log(newColor);
 }
 
 button.addEventListener("click", changeColor);
